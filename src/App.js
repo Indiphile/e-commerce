b490d5552b37347b4377ccd3f7d2b4a11631ed48
Indiphile <ingqambuza@gmail.com>
2022-09-07 03:50:19 +0200

    Initializing Development

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+
+      <Header/>
+
+      <main>
+        <h1>HELLO React</h1>
+      </main>
+
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <footer>
+                Footer_1
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
